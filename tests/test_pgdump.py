71e62778195143d52c0ba50ddf3982b5851d34df
Paco Ulloa <ulloa.paco@gmail.com>
2018-10-26 00:49:35 +0000

    pgdump module

--- tests/test_pgdump.py
+++ tests/test_pgdump.py
@@ -13,3 +13,11 @@ def test_dump_calls_pg_dump(mocker):
     assert pgdump.dump(url)
     subprocess.Popen.assert_called_with(['pg_dump', url], stdout=subprocess.PIPE)
 
+def test_dump_handles_oserror(mocker):
+    """
+    pgdump.dump returns a reasonable error if pg_dump isn't install
+    """
+    mocker.patch('subprocess.Popen', side_effect=OSError('no such file'))
+    with pytest.raises(SystemExit):
+        pgdump.dump(url)
+
